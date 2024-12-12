38ac4d79887e032a15b49226aaf794f3d0fe07cf
Chapin Bryce <mail@chapinb.com>
2024-12-12 18:55:51 -0500

    a - Ruff format

--- src/perimeter_scanner/aws_config.py
+++ src/perimeter_scanner/aws_config.py
@@ -80,7 +80,10 @@ class AwsConfig:
         self.aws_client = boto3.client("config")
 
     @staticmethod
-    def build_configuration(resource_type: ResourceType, configuration: str) -> Ec2Configuration | str:
+    def build_configuration(
+        resource_type: ResourceType,
+        configuration: str,
+    ) -> Ec2Configuration | str:
         try:
             configuration = json.loads(configuration)
         except json.JSONDecodeError:
@@ -88,49 +91,58 @@ class AwsConfig:
 
         if resource_type == ResourceType.EC2_Instance:
             return Ec2Configuration(
-                instance_id=configuration['instanceId'],
-                image_id=configuration['imageId'],
-                launch_time=datetime.fromisoformat(configuration['launchTime']),
-                tags=configuration['tags'],
-                platform_details=configuration['platformDetails'],
-                private_dns_name=configuration['privateDnsName'],
-                private_ip_address=ip_address(configuration['privateIpAddress']),
-                public_dns_name=configuration['publicDnsName'],
-                public_ip_address=ip_address(configuration['publicIpAddress']),
-                network_interfaces=configuration['networkInterfaces'],
-                security_groups=configuration['securityGroups'],
-                state=configuration['state'],
-                state_reason=configuration['stateReason'],
-                usage_operation=configuration['usageOperation'],
-                usage_operation_update_time=datetime.fromisoformat(configuration['usageOperationUpdateTime']),
-                subnet_id=configuration['subnetId'],
-                vpc_id=configuration['vpcId'],
+                instance_id=configuration["instanceId"],
+                image_id=configuration["imageId"],
+                launch_time=datetime.fromisoformat(configuration["launchTime"]),
+                tags=configuration["tags"],
+                platform_details=configuration["platformDetails"],
+                private_dns_name=configuration["privateDnsName"],
+                private_ip_address=ip_address(configuration["privateIpAddress"]),
+                public_dns_name=configuration["publicDnsName"],
+                public_ip_address=ip_address(configuration["publicIpAddress"]),
+                network_interfaces=configuration["networkInterfaces"],
+                security_groups=configuration["securityGroups"],
+                state=configuration["state"],
+                state_reason=configuration["stateReason"],
+                usage_operation=configuration["usageOperation"],
+                usage_operation_update_time=datetime.fromisoformat(
+                    configuration["usageOperationUpdateTime"]
+                ),
+                subnet_id=configuration["subnetId"],
+                vpc_id=configuration["vpcId"],
             )
         return configuration
 
-    def get_current_config_for_resource(self, resource_type: ResourceType, resource_id: str) -> ConfigItem | None:
+    def get_current_config_for_resource(
+        self,
+        resource_type: ResourceType,
+        resource_id: str,
+    ) -> ConfigItem | None:
         resp = self.aws_client.get_resource_config_history(
             resourceType=str(resource_type),
             resourceId=resource_id,
         )
 
         # get the first item, if any
-        config_items = resp.get('configurationItems')
+        config_items = resp.get("configurationItems")
         if not config_items or len(config_items) == 0:
             return None
 
         config_item = config_items[0]
 
-        config_resource_type = ResourceType(config_item['resourceType'])
+        config_resource_type = ResourceType(config_item["resourceType"])
         return ConfigItem(
             resource_type=config_resource_type,
-            resource_id=config_item['resourceId'],
-            resource_creation_time=config_item['resourceCreationTime'],
-            account=config_item['accountId'],
-            region=config_item['awsRegion'],
-            arn=config_item['arn'],
-            config_capture_time=config_item['configurationItemCaptureTime'],
-            config_status=config_item['configurationItemStatus'],
-            configuration=self.build_configuration(config_resource_type, config_item['configuration']),
-            tags=config_item['tags'],
+            resource_id=config_item["resourceId"],
+            resource_creation_time=config_item["resourceCreationTime"],
+            account=config_item["accountId"],
+            region=config_item["awsRegion"],
+            arn=config_item["arn"],
+            config_capture_time=config_item["configurationItemCaptureTime"],
+            config_status=config_item["configurationItemStatus"],
+            configuration=self.build_configuration(
+                config_resource_type,
+                config_item["configuration"],
+            ),
+            tags=config_item["tags"],
         )
