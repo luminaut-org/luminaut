806491cb0ce1e930068571f1211a83ed0ee695d5
Chapin Bryce <mail@chapinb.com>
2024-12-20 07:53:55 -0500

    F!! Split run into smaller methods

--- src/perimeter_scanner/luminaut.py
+++ src/perimeter_scanner/luminaut.py
@@ -21,9 +21,9 @@ default_progress_columns = [
 class Luminaut:
     def __init__(self, config: LuminautConfig):
         self.config = config
+        self.scanner = Scanner()
 
     def run(self):
-        scanner = Scanner()
         # Step 1: Enumerate ENIs with public IPs
         with progress.Progress(
             *default_progress_columns,
@@ -32,45 +32,61 @@ class Luminaut:
             task_id = task_progress.add_task(
                 "Enumerating ENIs with public IPs", total=None
             )
-            scan_results = scanner.aws_fetch_public_enis()
+            scan_results = self.scanner.aws_fetch_public_enis()
             task_progress.stop_task(task_id)
 
+            scan_results = self.gather_eni_context(scan_results, task_progress)
+
             for scan_result in scan_results:
-                # Step 2: Run the various tools that depend on the IP address
-                task_id = task_progress.add_task(
-                    f"Scanning {scan_result.ip} with nmap", total=None
-                )
-                nmap_results = scanner.nmap(scan_result.ip)
-                scan_result.findings.extend(nmap_results.findings)
-                task_progress.stop_task(task_id)
+                panel = scan_result.build_rich_panel()
+                console.print(panel)
 
-                task_id = task_progress.add_task(
-                    f"Checking AWS Config for {scan_result.eni_id}",
-                    total=None,
-                )
-                aws_config_results = scanner.aws_get_config_history_for_resource(
-                    models.ResourceType.EC2_NetworkInterface,
-                    scan_result.eni_id,
-                    scan_result.ip,
-                )
-                scan_result.findings.extend(aws_config_results.findings)
-                task_progress.stop_task(task_id)
+    def gather_eni_context(
+        self, scan_results: list[models.ScanResult], task_progress: progress.Progress
+    ):
+        updated_scan_results = []
+        for scan_result in scan_results:
+            # Step 2: Run the various tools that depend on the IP address
+            task_id = task_progress.add_task(
+                f"Scanning {scan_result.ip} with nmap", total=None
+            )
+            nmap_results = self.scanner.nmap(scan_result.ip)
+            scan_result.findings.extend(nmap_results.findings)
+            task_progress.stop_task(task_id)
 
-                for eni_resource in scan_result.get_eni_resources():
-                    # Scan for AWS config changes related to EC2 instances associated with an ENI
-                    if not eni_resource.ec2_instance_id:
-                        continue
+            scan_result = self.gather_aws_config_history(scan_result, task_progress)
+            updated_scan_results.append(scan_result)
 
-                    task_progress.add_task(
-                        f"Checking AWS Config for {eni_resource.ec2_instance_id}",
-                        total=None,
-                    )
-                    aws_config_results = scanner.aws_get_config_history_for_resource(
-                        models.ResourceType.EC2_Instance,
-                        eni_resource.ec2_instance_id,
-                        scan_result.ip,
-                    )
-                    scan_result.findings.extend(aws_config_results.findings)
+        return updated_scan_results
 
-            panel = scan_result.build_rich_panel()
-            console.print(panel)
+    def gather_aws_config_history(
+        self, scan_result: models.ScanResult, task_progress: progress.Progress
+    ) -> models.ScanResult:
+        task_id = task_progress.add_task(
+            f"Checking AWS Config for {scan_result.eni_id}",
+            total=None,
+        )
+        aws_config_results = self.scanner.aws_get_config_history_for_resource(
+            models.ResourceType.EC2_NetworkInterface,
+            scan_result.eni_id,
+            scan_result.ip,
+        )
+        scan_result.findings.extend(aws_config_results.findings)
+        task_progress.stop_task(task_id)
+
+        for eni_resource in scan_result.get_eni_resources():
+            # Scan for AWS config changes related to EC2 instances associated with an ENI
+            if not eni_resource.ec2_instance_id:
+                continue
+
+            task_progress.add_task(
+                f"Checking AWS Config for {eni_resource.ec2_instance_id}",
+                total=None,
+            )
+            aws_config_results = self.scanner.aws_get_config_history_for_resource(
+                models.ResourceType.EC2_Instance,
+                eni_resource.ec2_instance_id,
+                scan_result.ip,
+            )
+            scan_result.findings.extend(aws_config_results.findings)
+        return scan_result
