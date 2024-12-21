774ef5c640218a18d736c53285c92074dcfb26bd
Chapin Bryce <mail@chapinb.com>
2024-12-21 11:29:14 -0500

    t - Move config and client setup

--- tests/test_luminaut_core.py
+++ tests/test_luminaut_core.py
@@ -4,14 +4,18 @@ from luminaut import Luminaut, LuminautConfig, models
 
 
 class LuminautCore(unittest.TestCase):
+    def setUp(self):
+        self.config = LuminautConfig()
+        self.luminaut = Luminaut(self.config)
+
     def test_discover_public_ips_only_runs_if_aws_enabled(self):
-        config = LuminautConfig(aws=models.LuminautConfigToolAws(enabled=False))
-        luminaut = Luminaut(config)
-        scan_results = luminaut.discover_public_ips()
+        self.config.aws = models.LuminautConfigToolAws(enabled=False)
+        scan_results = self.luminaut.discover_public_ips()
         self.assertEqual([], scan_results)
 
         expected_result = models.ScanResult(ip="1.1.1.1", findings=[])
-        luminaut.scanner.aws_fetch_public_enis = lambda: [expected_result]
-        config.aws.enabled = True
-        scan_results = luminaut.discover_public_ips()
+        self.luminaut.scanner.aws_fetch_public_enis = lambda: [expected_result]
+        self.config.aws.enabled = True
+
+        scan_results = self.luminaut.discover_public_ips()
         self.assertEqual(expected_result, scan_results[0])
