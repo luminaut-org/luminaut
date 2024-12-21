480db1e499230a2a7a5992e73a8c2e91aa9a58a6
Chapin Bryce <mail@chapinb.com>
2024-12-21 12:00:23 -0500

    t - Add tests for the other cases (AWS: Enabled, Config: Disabled, and both enabled)

--- tests/test_luminaut_core.py
+++ tests/test_luminaut_core.py
@@ -38,18 +38,30 @@ class LuminautCore(unittest.TestCase):
         self.assertEqual(scan_findings, nmap_findings)
 
     def test_aws_config_only_runs_if_enabled(self):
+        test_finding = models.ScanFindings(tool="unittest")
         self.luminaut.scanner.aws_get_config_history_for_resource = (
             lambda *args: models.ScanResult(
                 ip="10.0.0.1",
-                findings=[models.ScanFindings(tool="unittest")],
+                findings=[test_finding],
             )
         )
 
         self.config.aws = models.LuminautConfigToolAws(enabled=False)
         self.config.aws.config = models.LuminautConfigTool(enabled=True)
-
         scan_result = self.luminaut.gather_aws_config_history(
             models.ScanResult(ip="10.0.0.1", findings=[])
         )
-
         self.assertEqual([], scan_result.findings)
+
+        self.config.aws.enabled = True
+        self.config.aws.config = models.LuminautConfigTool(enabled=False)
+        scan_result = self.luminaut.gather_aws_config_history(
+            models.ScanResult(ip="10.0.0.1", findings=[])
+        )
+        self.assertEqual([], scan_result.findings)
+
+        self.config.aws.config.enabled = True
+        scan_result = self.luminaut.gather_aws_config_history(
+            models.ScanResult(ip="10.0.0.1", findings=[])
+        )
+        self.assertEqual([test_finding], scan_result.findings)
