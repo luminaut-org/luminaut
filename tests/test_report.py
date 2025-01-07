878b9fcfbe1ff8c9a4dce21b473089c6d500f0c9
Chapin Bryce <mail@chapinb.com>
2025-01-06 19:50:41 -0500

    F!! Add CSV timeline capability across scans and tool findings.

--- tests/test_report.py
+++ tests/test_report.py
@@ -1,12 +1,18 @@
 import copy
+import csv
 import unittest
-from datetime import datetime
+from datetime import UTC, datetime
 from io import StringIO
 
 import orjson
 
 from luminaut import models
-from luminaut.report import write_json_report, write_jsonl_report
+from luminaut.report import (
+    timeline_columns,
+    write_csv_timeline,
+    write_json_report,
+    write_jsonl_report,
+)
 
 
 class JsonReport(unittest.TestCase):
@@ -71,3 +77,57 @@ class JsonReport(unittest.TestCase):
 
             self.assertIsInstance(json_result, dict)
             self.assertEqual(scan_result.ip, json_result["ip"])
+
+    def test_generate_timeline_report(self):
+        first_date = datetime(2025, 1, 1, 0, 0, 0, 0, UTC)
+        second_date = datetime(2025, 1, 1, 0, 0, 1, 0, UTC)
+        third_date = datetime(2025, 1, 1, 0, 0, 2, 0, UTC)
+        finding1 = models.ScanFindings(
+            tool="foo",
+            events=[
+                models.TimelineEvent(
+                    timestamp=second_date,
+                    source="foo",
+                    event_type=models.TimelineEventType.COMPUTE_INSTANCE_CREATED,
+                    resource_id="i-0",
+                    resource_type=models.ResourceType.EC2_Instance,
+                    message="foo message",
+                    details={"foo details": "bar"},
+                ),
+                models.TimelineEvent(
+                    timestamp=third_date,
+                    source="unittest",
+                    event_type=models.TimelineEventType.COMPUTE_INSTANCE_TERMINATED,
+                    resource_id="i-0",
+                    resource_type=models.ResourceType.EC2_Instance,
+                ),
+            ],
+        )
+        finding2 = models.ScanFindings(
+            tool="bar",
+            events=[
+                models.TimelineEvent(
+                    timestamp=first_date,
+                    source="bar",
+                    event_type=models.TimelineEventType.COMPUTE_INSTANCE_CREATED,
+                    resource_id="i-1",
+                    resource_type=models.ResourceType.EC2_Instance,
+                ),
+            ],
+        )
+        scan = models.ScanResult(ip="10.0.0.1", findings=[finding1, finding2])
+
+        timeline = StringIO("r+")
+        write_csv_timeline([scan], timeline)
+
+        timeline.seek(0)
+        reader = csv.DictReader(timeline)
+        lines = list(reader)
+        headers = list(lines[0])
+        self.assertEqual(len(lines), 3)
+        for column in timeline_columns:
+            self.assertIn(column, headers)
+
+        self.assertIn(str(first_date), lines[0]["timestamp"])
+        self.assertIn(str(second_date), lines[1]["timestamp"])
+        self.assertIn(str(third_date), lines[2]["timestamp"])
