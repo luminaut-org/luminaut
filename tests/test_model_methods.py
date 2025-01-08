a5ee81aa0f50ef5902a182cc1625b9570164c862
Chapin Bryce <mail@chapinb.com>
2025-01-08 12:30:12 -0500

    F - Ignore these type issues, as the values are arbitrary

--- tests/test_model_methods.py
+++ tests/test_model_methods.py
@@ -6,9 +6,9 @@ from luminaut import models
 class TestModels(unittest.TestCase):
     def test_scan_finding_bool(self):
         self.assertFalse(bool(models.ScanFindings(tool="foo")))
-        self.assertTrue(bool(models.ScanFindings(tool="foo", resources=["bar"])))
-        self.assertTrue(bool(models.ScanFindings(tool="foo", services=["bar"])))
-        self.assertTrue(bool(models.ScanFindings(tool="foo", events=["bar"])))
+        self.assertTrue(bool(models.ScanFindings(tool="foo", resources=["bar"])))  # type: ignore
+        self.assertTrue(bool(models.ScanFindings(tool="foo", services=["bar"])))  # type: ignore
+        self.assertTrue(bool(models.ScanFindings(tool="foo", events=["bar"])))  # type: ignore
 
 
 if __name__ == "__main__":
