1c37a1b13117147c4efdbf3bc7403a763bd49aef
Chapin Bryce <mail@chapinb.com>
2025-06-22 10:36:01 -0400

    F - Remove unused CLI interface

--- src/luminaut/tools/gcp.py
+++ src/luminaut/tools/gcp.py
@@ -1,4 +1,3 @@
-import argparse
 import logging
 
 from google.cloud import compute_v1
@@ -69,39 +68,3 @@ class Gcp:
             zone=zone,
         )
         return [models.GcpInstance.from_gcp(instance) for instance in instances]
-
-
-if __name__ == "__main__":
-    args = argparse.ArgumentParser(description="GCP Instance Manager")
-    args.add_argument(
-        "project",
-        type=str,
-        help="GCP project ID",
-    )
-    args.add_argument(
-        "zone",
-        type=str,
-        help="GCP zone",
-    )
-    cli_args = args.parse_args()
-
-    config = models.LuminautConfig(
-        gcp=models.LuminautConfigToolGcp(
-            enabled=True,
-            projects=[cli_args.project],
-            compute_zones=[cli_args.zone],
-        )
-    )
-
-    client = compute_v1.InstancesClient()
-    instances = Gcp(config, gcp_client=client).fetch_instances(
-        project=config.gcp.projects[0],
-        zone=config.gcp.compute_zones[0],
-    )
-    for instance in instances:
-        print(f"Instance Name: {instance.name}")
-        print(f"Instance ID: {instance.resource_id}")
-        print(f"Status: {instance.status}")
-        print(f"Zone: {instance.zone}")
-        print(f"Created Time: {instance.creation_time}")
-        print(f"External IP: {instance.network_interfaces[0].public_ip}")
