0b185113476a199150a53bd6138f1b7ca660bb84
Chapin Bryce <mail@chapinb.com>
2025-07-08 09:01:08 -0400

    t - Add a test for hostname support

--- tests/test_scan_ip.py
+++ tests/test_scan_ip.py
@@ -36,3 +36,41 @@ def test_nmap():
     assert "nmap" == nmap_results.findings[0].tool
     assert isinstance(nmap_results.findings[0].services[0], models.NmapPortServices)
     assert "foo" == nmap_results.findings[0].services[0].name
+
+
+def test_nmap_hostname():
+    hostname = "example.com"
+    service_name = "http"
+    service_product = "nginx"
+    service_version = "1.20.1"
+    nmap_response = {
+        hostname: {
+            "ports": [
+                {
+                    "portid": "80",
+                    "protocol": models.Protocol.TCP,
+                    "reason": "syn-ack",
+                    "service": {
+                        "name": service_name,
+                        "product": service_product,
+                        "version": service_version,
+                    },
+                    "state": "open",
+                }
+            ]
+        }
+    }
+
+    with patch("luminaut.scanner.nmap3") as mocked_nmap3:
+        mocked_nmap3.Nmap().nmap_version_detection.return_value = nmap_response
+        nmap_results = Scanner(config=models.LuminautConfig()).nmap(hostname)
+
+    assert mocked_nmap3.Nmap().nmap_version_detection.called_once()
+    assert (
+        hostname == nmap_results.ip
+    )  # Currently expecting IP field to contain hostname
+    assert "nmap" == nmap_results.findings[0].tool
+    assert isinstance(nmap_results.findings[0].services[0], models.NmapPortServices)
+    assert "http" == nmap_results.findings[0].services[0].name
+    assert "nginx" == nmap_results.findings[0].services[0].product
+    assert "1.20.1" == nmap_results.findings[0].services[0].version
